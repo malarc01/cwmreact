cf91c33f405b5dc4c93bf82afe4f412afc23f59c
Your Name <melky@Melkys-iMac.local>
2019-08-25 12:25:02 -0400

    added handleDelelete funciton & setState & table render()

--- vid/src/comp/movie.jsx
+++ vid/src/comp/movie.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const divStyle = {
-	margin: '40px',
-	border: '5px solid red'
+  margin: "40px",
+  border: "5px solid red"
 };
 
 class Movie extends Component {
-	render() {
-		// const { moviesArray } = this.props;
-		return (
-			<div style={divStyle}>
-				<h3>Movie Comp</h3>
-				<h3>⚔️ blank=>{this.props.gjpoaspjodgsdgjops}</h3>
-				<h3>
-					Render=> {'=> => '}
-					{this.props.filmArray.map((film) => {
-						return film.title;
-					})}
-				</h3>
-			</div>
-		);
-	}
+  render() {
+    // const { moviesArray } = this.props;
+    return (
+      <div style={divStyle}>
+        <h3>Movie Comp</h3>
+        <h3>⚔️ blank=>{this.props.gjpoaspjodgsdgjops}</h3>
+        <h3>
+          Render=> {"=> => "}
+          {this.props.filmArray.map(film => {
+            return film.title;
+          })}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default Movie;
--- vid/src/comp/movies.jsx
+++ vid/src/comp/movies.jsx
@@ -1,31 +1,69 @@
-import React, { Component } from 'react';
-import Movie from './movie';
+import React, { Component } from "react";
+import Movie from "./movie";
 
-import { getMovies } from '../services/fakeMovieService';
+import { getMovies } from "../services/fakeMovieService";
 
 const divStyle = {
-	margin: '40px',
-	border: '5px solid pink'
+  margin: "40px",
+  border: "5px solid pink"
 };
 
 class Movies extends Component {
-	state = {
-		movies: getMovies()
-	};
+  state = {
+    movies: getMovies()
+  };
 
-	handleDelete = (movie) => {
-		console.log(movie);
-	};
-	render() {
-		return (
-			<div style={divStyle}>
-				<h2>movies.jsx</h2>
-				{this.state.movies.map((movie) => <Movie key={movie.id} filmArray={this.state.movies} />)}
-			</div>
-		);
+  handleDelete = movie => {
+    console.log(movie);
+  };
+  handleDelete = film => {
+    console.log(film);
 
-		// return <Movie moviesArray={this.state.movies} />;
-	}
+    const newMoviesArray = this.state.movies.filter(m => m._id !== film._id);
+    this.setState({ movies: newMoviesArray });
+
+    console.log("💕");
+  };
+  render() {
+    return (
+      <div style={divStyle}>
+        <h2>movies.jsx</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(filmObject => {
+              return (
+                <tr key={filmObject._id}>
+                  <td>{filmObject.title}</td>
+                  <td>{filmObject.genre.name}</td>
+                  <td>{filmObject.numberInStock}</td>
+                  <td>{filmObject.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={click => this.handleDelete(filmObject)}
+                      className="btn  btn-danger btn-sm "
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+
+    // return <Movie moviesArray={this.state.movies} />;
+  }
 }
 
 export default Movies;
