648d0c500c772fdb8ab429ad3e8ee22fa3eda4b0
Your Name <melky@Melkys-iMac.local>
2019-08-25 13:21:07 -0400

    add conditonal render on Zero + padding to comp

--- vid/src/comp/movies.jsx
+++ vid/src/comp/movies.jsx
@@ -17,17 +17,30 @@ class Movies extends Component {
     console.log(movie);
   };
   handleDelete = film => {
-    console.log(film);
+    console.log("film selected for deletion is => ", film);
 
-    const newMoviesArray = this.state.movies.filter(m => m._id !== film._id);
+    const newMoviesArray = this.state.movies.filter(
+      m =>
+        // console.log("MMMMMMMMMMMMMMMMMM is =>", m);
+        m._id !== film._id
+    );
     this.setState({ movies: newMoviesArray });
 
     console.log("💕");
   };
   render() {
+    // const filmLength = this.state.movies.length;
+    //  filmLength destructing below
+    const { length: count } = this.state.movies;
+    if (count === 0) {
+      console.log("ZERO");
+      return <p>No Movies</p>;
+    }
     return (
       <div style={divStyle}>
         <h2>movies.jsx</h2>
+
+        <p>Showing {count} movies in the database</p>
         <table className="table">
           <thead>
             <tr>
