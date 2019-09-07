5342216967a46444471326c62964375c17d914a8
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-09-07 09:38:56 -0400

    add valdiateProperty func

--- vid/src/comp/loginForm.jsx
+++ vid/src/comp/loginForm.jsx
@@ -35,8 +35,8 @@ class LoginForm extends Component {
 		e.preventDefault();
 
 		const errors = this.validate();
-		console.log(errors);
-		this.setState({ errors });
+		// console.log(errors);
+		this.setState({ errors: errors || {} });
 		if (errors) return;
 
 		// Call the server
@@ -44,24 +44,49 @@ class LoginForm extends Component {
 
 		console.log('Submitted');
 	};
-
+	validateProperty = ({ name, value }) => {
+		if (name === 'username') {
+			if (value.trim() === '') return 'Username is required';
+			// ... other rules
+		}
+		if (name === 'username') {
+			if (value.trim() === '') return 'Password is required';
+		}
+	};
 	handleChange = ({ currentTarget: input }) => {
-		console.log('ZUM', input);
+		const errors = { ...this.state.errors };
+		const errorMessage = this.validateProperty(input);
+		if (errorMessage) errors[input.name] = errorMessage;
+		else delete errors[input.name];
+
+		console.log('INPUT IS =>', input);
 		const account = { ...this.state.account };
 		account[input.name] = input.value;
-		console.log();
-		this.setState({ account });
+
+		this.setState({ account, errors });
 		console.log('update');
 	};
 
 	render() {
-		const { account } = this.state;
+		const { account, errors } = this.state;
 		return (
 			<div>
 				<h1>Login</h1>
 				<form onSubmit={this.handleSubmit}>
-					<Input name='username' value={account.username} label='Username' onChange={this.handleChange} />
-					<Input name='password' value={account.password} label='Password' onChange={this.handleChange} />
+					<Input
+						name='username'
+						value={account.username}
+						label='Username'
+						onChange={this.handleChange}
+						error={errors.username}
+					/>
+					<Input
+						name='password'
+						value={account.password}
+						label='Password'
+						onChange={this.handleChange}
+						error={errors.password}
+					/>
 					{/* <div className='form-group'>
 						<label htmlFor='password'>Password</label>
 						<input
