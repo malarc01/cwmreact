8cb83fee20eb85411ba43cc27693db3ce6389040
Your Name <melky@Melkys-iMac.local>
2019-09-08 12:22:24 -0400

    add registForm.jsx

--- vid/src/comp/common/form.jsx
+++ vid/src/comp/common/form.jsx
@@ -1,104 +1,109 @@
-import React, { Component } from 'react';
-import Joi from 'joi-browser';
-import Input from './input';
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./input";
 
 class Form extends Component {
-	state = {
-		data: {},
-		errors: {}
-	};
+  state = {
+    data: {},
+    errors: {}
+  };
 
-	validate = () => {
-		const options = { abortEarly: false };
-		const { error } = Joi.validate(this.state.data, this.schema, options);
+  validate = () => {
+    console.log("validate () invoked");
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
 
-		if (!error) return null;
+    if (!error) return null;
 
-		const errors = {};
-		// maps array into a Object try with map and reduce methods
-		for (let item of error.details) errors[item.path[0]] = item.message;
-		return errors;
+    const errors = {};
+    // maps array into a Object try with map and reduce methods
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
 
-		// console.log('OUTPUT =>: validate -> result', result);
+    // console.log('OUTPUT =>: validate -> result', result);
 
-		// const errors = {};
+    // const errors = {};
 
-		// const { data } = this.state;
+    // const { data } = this.state;
 
-		// if (data.username.trim() === '') errors.username = 'Username is required.';
-		// if (data.password.trim() === '') errors.password = 'Password is required.';
-		// return Object.keys(errors).length === 0 ? null : errors;
-	};
+    // if (data.username.trim() === '') errors.username = 'Username is required.';
+    // if (data.password.trim() === '') errors.password = 'Password is required.';
+    // return Object.keys(errors).length === 0 ? null : errors;
+  };
 
-	validateProperty = ({ name, value }) => {
-		const obj = { [name]: value };
-		const schema = { [name]: this.schema[name] };
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    console.log("obj => ", obj);
+    // console.log("schema 1st => ", schema);
+    const schema = { [name]: this.schema[name] };
+    console.log("schema => ", schema);
 
-		const { error } = Joi.validate(obj, schema);
-		return error ? error.details[0].message : null;
+    const { error } = Joi.validate(obj, schema);
+    console.log("error => ", error);
+    return error ? error.details[0].message : null;
 
-		// if (name === 'username') {
-		// 	if (value.trim() === '') return 'Username is required';
-		// 	// ... other rules
-		// }
-		// if (name === 'username') {
-		// 	if (value.trim() === '') return 'Password is required';
-		// }
-	};
+    // if (name === 'username') {
+    // 	if (value.trim() === '') return 'Username is required';
+    // 	// ... other rules
+    // }
+    // if (name === 'username') {
+    // 	if (value.trim() === '') return 'Password is required';
+    // }
+  };
 
-	handleSubmit = (e) => {
-		e.preventDefault();
+  handleSubmit = e => {
+    e.preventDefault();
 
-		const errors = this.validate();
-		// console.log(errors);
-		this.setState({ errors: errors || {} });
-		if (errors) return;
+    const errors = this.validate();
+    // console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
 
-		// Call the server
-		// const username = this.username.current.value;
+    // Call the server
+    // const username = this.username.current.value;
 
-		this.doSubmit();
-	};
+    this.doSubmit();
+  };
 
-	handleChange = ({ currentTarget: input }) => {
-		const errors = { ...this.state.errors };
-		const errorMessage = this.validateProperty(input);
-		if (errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
 
-		console.log('INPUT IS =>', input);
+    console.log("INPUT IS =>", input);
 
-		const data = { ...this.state.data };
-		data[input.name] = input.value;
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
 
-		this.setState({ data, errors });
-		console.log('update');
-	};
+    this.setState({ data, errors });
+    console.log("update");
+  };
 
-	renderButton(label) {
-		return (
-			<button disabled={this.validate()} className='btn btn-primary'>
-				{label}
-			</button>
-		);
-	}
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
 
-	renderInput(name, label, type = 'text') {
-		console.log('renderInput(NAME, LABEL, TYPE) =>', name, label, type);
+  renderInput(name, label, type = "text") {
+    console.log("renderInput(NAME, LABEL, TYPE) =>", name, label, type);
 
-		const { data, errors } = this.state;
+    const { data, errors } = this.state;
 
-		return (
-			<Input
-				type={type}
-				name={name}
-				value={data[name]}
-				label={label}
-				onChange={this.handleChange}
-				error={errors[name]}
-			/>
-		);
-	}
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 }
 
 export default Form;
--- vid/src/comp/registerForm.jsx
+++ vid/src/comp/registerForm.jsx
@@ -1,10 +1,48 @@
-import React, { Component } from 'react';
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class RegisterForm extends Component {
-	state = {};
-	render() {
-		return <h1>Register</h1>;
-	}
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+    // x:{}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  // validate = () =>{
+  // 	const options =
+  // }
+
+  doSubmit = () => {
+    // call the server
+    console.log("Submitted");
+  };
+
+  // renderButton(label) {
+  //   return <button className={label} disabled={this.validate()}></button>;
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Pword", "password")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default RegisterForm;
